711fa2a296cbf8df030398c4b508bf2f0da2cd8b
Jose Serra <148002646+jofran93@users.noreply.github.com>
2023-11-26 23:46:15 -0300

    Delete PlaygroundFinalProject-Serra/MercadoWeb directory

--- PlaygroundFinalProject-Serra/MercadoWeb/TuFeriaCL/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile,Item,Post,Purchase
-
-
-admin.site.register(UserProfile)
-admin.site.register(Item)
-admin.site.register(Post)
-admin.site.register(Purchase)
